9d4cbd06c9aa4f7b5b9b5c41aa85df74ae1b8ed5
Álvaro Oliveira <alvarogab6@gmail.com>
2019-12-30 14:47:00 -0300

    fixes page chat

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-const Dashboard = () => (
-    <div>
-        Chat
+import List from './List';
+import Messenger from './Messenger';
+
+import './chat.css';
+
+const Chat = () => (
+    <div className="container-fluid h-100">
+        <div className="row h-100">
+            <div className="col-4 h-100 scroll">
+                <List />
+            </div>
+            <div className="col-8 p-0 h-100 scroll">
+                <Messenger />
+                {/*<div className="d-flex justify-content-center align-items-center h-100 message-alert">
+                    Selecione uma conversa
+                </div>*/}
+            </div>
+        </div>
     </div>
 );
 
-export default Dashboard;
+export default Chat;
