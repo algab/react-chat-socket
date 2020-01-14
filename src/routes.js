f51a1f69b1bb47ba8a5c9d2a702c7d50210a9532
Álvaro Oliveira <alvarogab6@gmail.com>
2020-01-14 15:44:03 -0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,35 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Chat from './pages/Chat';
 
+const GuardPublic = ({ path, component, ...rest }) => {
+    const user = localStorage.getItem('user');
+    if (user) {
+        return <Redirect to="/chat" />
+    } else {
+        return <Route path={path} component={component} {...rest} />
+    }
+};
+
+const GuardPrivate = ({ path, component, ...rest }) => {
+    const user = localStorage.getItem('user');
+    if (user) {
+        return <Route path={path} component={component} {...rest} />
+    } else {
+        return <Redirect to="/" />
+    }
+};
+
 const Routes = () => (
     <BrowserRouter>
         <Switch>
-            <Route path="/" exact={true} component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/chat" component={Chat} />
+            <GuardPublic path="/" exact component={Login} />
+            <GuardPublic path="/register" component={Register} />
+            <GuardPrivate path="/chat" component={Chat} />
+            <Redirect from="*" to="/" />
         </Switch>
     </ BrowserRouter>
 );
