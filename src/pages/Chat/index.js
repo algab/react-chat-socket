6c5a74d2124c8029c207126994c76bea77cf10df
Álvaro Oliveira <alvarogab6@gmail.com>
2020-01-14 15:43:44 -0300

    fix page Chat

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import List from './List';
 import Messenger from './Messenger';
 
 import './chat.css';
 
-const Chat = () => (
-    <div className="container-fluid h-100">
-        <div className="row h-100">
-            <div className="col-4 h-100 scroll">
-                <List />
-            </div>
-            <div className="col-8 p-0 h-100 scroll">
-                <Messenger />
-                {/*<div className="d-flex justify-content-center align-items-center h-100 message-alert">
-                    Selecione uma conversa
-                </div>*/}
-            </div>
-        </div>
-    </div>
-);
+export default class Chat extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { toUser: undefined };
+    }
 
-export default Chat;
+    select = (toUser) => () => {
+        this.setState({ toUser });
+    };
+
+    render() {
+        const { toUser } = this.state;
+        return (
+            <div className="container-fluid h-100">
+                <div className="row h-100">
+                    <div className="col-4 h-100 scroll">
+                        <List select={this.select} history={this.props.history} />
+                    </div>
+                    <div className="col-8 p-0 h-100 scroll">
+                        {toUser === undefined ?
+                            (
+                                <div className="d-flex justify-content-center align-items-center h-100 message-alert">
+                                    Selecione uma conversa
+                                </div>
+                            ) : <Messenger toUser={toUser} />
+                        }
+                    </div>
+                </div>
+            </div>
+        )
+    }
+};
