2192111cb4cf29d546fc39acfd9ca71a32682508
Álvaro Oliveira <alvarogab6@gmail.com>
2020-01-15 16:05:22 -0300

    change .env

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,5 +1,5 @@
 import socketIOClient from 'socket.io-client';
 
-const socket = socketIOClient('http://localhost:4000');
+const socket = socketIOClient(process.env.REACT_APP_SOCKET_URL);
 
 export default socket;
